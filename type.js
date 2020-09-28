5a7127ee17a70383dc5b0c7321c06f44d60d6034
riho <rihoriho1031@gmail.com>
2020-09-28 17:16:16 +0900

    shuffle array

--- type.js
+++ type.js
@@ -1,9 +1,40 @@
-import { questionList } from "./const.js";
+let countriesList = [
+  "アフガニスタン",
+  "アルバニア",
+  "アルジェリア",
+  "アンゴラ",
+  "アルゼンチン",
+  "アルメニア",
+  "オーストラリア",
+  "オーストリア",
+  "アゼルバイジャン",
+  "バハマ",
+  "バーレーン",
+  "バングラデシュ",
+  "バルバドス",
+  "ベラルーシ",
+  "ベルギー",
+  "ブータン",
+  "ボリビア",
+  "カンボジア",
+  "カメルーン",
+  "カナダ",
+  "チリ",
+  "中国",
+  "コロンビア",
+  "コンゴ",
+  "コスタリカ",
+  "メキシコ",
+  "インドネシア",
+  "タイ",
+  "ラオス",
+  "フランス",
+];
 
-// {
-//   document.addEventListener("keydown", (e) => {
-//     console.log(e);
-//   });
-// }
-
-console.log(questionList);
+//配列の値をシャッフルする
+for (i = countriesList.length - 1; i > 0; i--) {
+  let j = Math.floor(Math.random() * (i + 1));
+  let tmp = countriesList[i];
+  countriesList[i] = countriesList[j];
+  countriesList[j] = tmp;
+}
